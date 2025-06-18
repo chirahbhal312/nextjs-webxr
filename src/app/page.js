cd274ba3eedbc6880e661a4808ffe04400e32dc8
chirahbhal312 <chiragbhal312@gmail.com>
2025-06-18 14:47:21 +0530

    conversion trial 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,23 @@
-'use client'
-
 import { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { XR, createXRStore } from '@react-three/xr'
+import ARViewer from '../components/ARViewer'
 
 export default function Home() {
-  const [red, setRed] = useState(false)
-  const store = createXRStore()
+  const [url, setUrl] = useState('')
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <button
-        style={{
-          position: 'absolute',
-          zIndex: 1,
-          top: 20,
-          left: '50%',
-          transform: 'translateX(-50%)',
-          padding: '0.5rem 1rem',
+    <div>
+      <input
+        type="file"
+        accept=".glb,.gltf"
+        onChange={e => {
+          const file = e.target.files[0]
+          if (!file) return
+          const blobURL = URL.createObjectURL(file)
+          setUrl(blobURL)
         }}
-        onClick={() => store.enterAR()}
-      >
-        Enter AR
-      </button>
-
-      <Canvas>
-        <XR store={store} sessionInit={{ requiredFeatures: ['hit-test'] }}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-
-          <mesh
-            position={[0, 0, -1]}
-            onClick={() => setRed((s) => !s)}
-          >
-            <boxGeometry args={[0.2, 0.2, 0.2]} />
-            <meshStandardMaterial color={red ? 'red' : 'blue'} />
-          </mesh>
-        </XR>
-      </Canvas>
+        style={{ position: 'absolute', top: 10, left: 10, zIndex: 10 }}
+      />
+      <ARViewer url={url} />
     </div>
   )
 }
